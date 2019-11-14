6a912a07331689e15e9634bace6f328276293f61
Vladimir Veljkovic <veljkovicvladimir86@gmail.com>
2019-11-14 17:04:14 +0100

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { StoreFirstGuard } from './store/storeFirst.guard';
+import { StoreModule } from './store/store.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule
+  ],
+  providers: [StoreFirstGuard],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/model/model.module.ts
@@ -0,0 +1,24 @@
+import { AuthService } from './auth.service';
+import { Cart } from './cart.model';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { Order } from './order.model';
+import { OrderRepository } from './order.repository';
+import { ProductRepository } from './product.repository';
+import { RestDataSource } from './rest.datasource';
+import { StaticDataSource } from './static.datasource';
+
+@NgModule({
+  imports: [HttpClientModule],
+  providers: [
+    ProductRepository,
+    StaticDataSource,
+    Cart,
+    Order,
+    OrderRepository,
+    { provide: StaticDataSource, useClass: RestDataSource },
+    RestDataSource,
+    AuthService
+  ]
+})
+export class ModelModule {}
